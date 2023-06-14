6778288458b78c173fac9699bea24e0f4403aa27
unknown <javierchoquetico@gmail.com>
2023-06-14 16:01:07 -0500

    refactorizacion en el custom drawer

--- lib/components/widgets/custom_drawer.dart
+++ lib/components/widgets/custom_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:hablandohuevadasf/models/reqres_merch.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -39,100 +40,9 @@ class CustomDrawer extends StatelessWidget {
                       merch.length,
                       (i) {
                         final merchs = merch[i];
-                        return Container(
-                          margin: const EdgeInsets.all(6),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(14),
-                            color: Colors.white,
-                            boxShadow: [
-                              BoxShadow(
-                                  color: Colors.black.withOpacity(0.2),
-                                  offset: const Offset(4, 4),
-                                  blurRadius: 12),
-                            ],
-                          ),
-                          child: Column(
-                            children: [
-                              Expanded(
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(14),
-                                  child: FadeInImage(
-                                    fit: BoxFit.cover,
-                                    placeholder:
-                                        const AssetImage('assets/no-image.png'),
-                                    image: NetworkImage(merchs.image),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8),
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      merchs.name,
-                                      style: const TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                    const SizedBox(
-                                      height: 3,
-                                    ),
-                                    Text(
-                                      'S/${merchs.price.toDouble().toStringAsFixed(2)}',
-                                      style: const TextStyle(
-                                          color: Colors.red,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    const SizedBox(
-                                      height: 5,
-                                    ),
-                                    SizedBox(
-                                      height: 22,
-                                      child: merchs.status
-                                          ? Bounce(
-                                              infinite: true,
-                                              from: 5,
-                                              delay: Duration(
-                                                  milliseconds: 300 * i),
-                                              child: FilledButton(
-                                                style: FilledButton.styleFrom(
-                                                  backgroundColor: Colors.green,
-                                                ),
-                                                onPressed: () async {
-                                                  final url =
-                                                      Uri.parse(merchs.url);
-                                                  if (!await launchUrl(url)) {
-                                                    throw 'No se pudo lanzar el $url';
-                                                  }
-                                                },
-                                                child: const Text(
-                                                  'COMPRAR',
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                ),
-                                              ),
-                                            )
-                                          : FilledButton(
-                                              onPressed: () {},
-                                              style: FilledButton.styleFrom(
-                                                  backgroundColor: Colors.red),
-                                              child: const Text(
-                                                'AGOTADO',
-                                                style: TextStyle(
-                                                  color: Colors.white,
-                                                  fontWeight: FontWeight.bold,
-                                                ),
-                                              ),
-                                            ),
-                                    ),
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
+                        return _MerchCard(
+                          merchs: merchs,
+                          index: i,
                         );
                       },
                     ),
@@ -142,6 +52,110 @@ class CustomDrawer extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _MerchCard extends StatelessWidget {
+  const _MerchCard({
+    required this.merchs,
+    required this.index,
+  });
+
+  final Merch merchs;
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(6),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(14),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.2),
+              offset: const Offset(4, 4),
+              blurRadius: 12),
+        ],
+      ),
+      child: Column(
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(14),
+              child: FadeInImage(
+                fit: BoxFit.cover,
+                placeholder: const AssetImage('assets/no-image.png'),
+                image: NetworkImage(merchs.image),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              children: [
+                Text(
+                  merchs.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  'S/${merchs.price.toDouble().toStringAsFixed(2)}',
+                  style: const TextStyle(
+                      color: Colors.red, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                SizedBox(
+                  height: 22,
+                  child: merchs.status
+                      ? Bounce(
+                          infinite: true,
+                          from: 5,
+                          delay: Duration(milliseconds: 300 * index),
+                          child: FilledButton(
+                            style: FilledButton.styleFrom(
+                              backgroundColor: Colors.green,
+                            ),
+                            onPressed: () async {
+                              final url = Uri.parse(merchs.url);
+                              if (!await launchUrl(url)) {
+                                throw 'No se pudo lanzar el $url';
+                              }
+                            },
+                            child: const Text(
+                              'COMPRAR',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        )
+                      : FilledButton(
+                          onPressed: () {},
+                          style: FilledButton.styleFrom(
+                              backgroundColor: Colors.red),
+                          child: const Text(
+                            'AGOTADO',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
